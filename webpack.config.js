4a5ffabc5956ab3cc4491e39364769998d08f29c
KochanovNikita <kochanovn207@gmail.com>
2022-06-05 18:45:46 +0300

    init: init project

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+  }),
+  new MiniCssExtractPlugin({
+    filename: 'style/[name].[contenthash].css', // Формат имени файла
+  }),
+];
+
+module.exports = {
+  mode,
+  plugins,
+  entry: './src/assets/js/index.js',
+  devtool: 'source-map',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[hash][ext][query]',
+    clean: true,
+  },
+  devServer: {
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/, use: ['html-loader'],
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ico)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
