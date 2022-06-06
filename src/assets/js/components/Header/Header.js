0d27e7e3e693a4467cab87c1d5c357ac8cd6570a
KochanovNikita <kochanovn207@gmail.com>
2022-06-06 21:10:13 +0300

    feat: add container, add Header

--- src/assets/js/components/Header/Header.js
+++ src/assets/js/components/Header/Header.js
@@ -5,10 +5,21 @@ import './Header.scss';
 const Header = () => {
   const header = `
     <header class="header">
-      <h2 class="header__logo">
-        <img src="${logo}"/>
-        MovieApp
-      </h2>
+      <div class="container">
+        <div class="header__wrapper">
+          <h2 class="header__logo">
+            <img src="${logo}"/>
+            MovieApp
+          </h2>
+          <nav class="nav">
+            <ul class="nav__list">
+              <li class="nav__item">Главная</li>
+              <li class="nav__item">Топ 250</li>
+              <li class="nav__item">Топ 100</li>
+              <li class="nav__item">Сейчас ждут</li>
+            </ul>
+        </div>
+      </div>
     </header>
   `;
   APP.innerHTML += header;
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,7 @@
 import Header from './components/Header/Header';
 import { API_URI_V2, TOP_250 } from './constants';
 import getData from './getData';
+import '../style/style.css';
 
 const createMovieTemplate = async (movies) => {
   console.log(movies);
