bb06f7fdd7b1590947f86f15dad48d2105094bd6
KochanovNikita <kochanovn207@gmail.com>
2022-06-09 21:57:14 +0300

    fix: change header

--- src/assets/js/components/Header/Header.js
+++ src/assets/js/components/Header/Header.js
@@ -1,24 +1,34 @@
 import logo from '../../../img/film.png';
 import { APP } from '../../constants';
 import './Header.scss';
+import './Form.scss';
 
 const Header = () => {
   const header = `
     <header class="header">
       <div class="container">
         <div class="header__wrapper">
-          <h2 class="header__logo">
+          <a href="index.html" class="header__logo">
             <img src="${logo}"/>
             MovieApp
-          </h2>
-          <nav class="nav">
-            <ul class="nav__list">
-              <li class="nav__item">Главная</li>
-              <li class="nav__item">Топ 250</li>
-              <li class="nav__item">Топ 100</li>
-              <li class="nav__item">Сейчас ждут</li>
-            </ul>
-        </div>
+          </a>
+          <div class="header__menu">
+            <div class="header__burger">
+              <div class="header__element"></div>
+              <div class="header__element"></div>
+              <div class="header__element"></div>
+            </div>
+            <div class="header__title">
+              Menu
+            </div>
+          </div>
+          <form class="form" action="index.html">
+            <input class="form__search" type="text">
+            <button class="form__submit">
+              <span class="icon-search"></span>
+            </button>
+          </form>
+
       </div>
     </header>
   `;
